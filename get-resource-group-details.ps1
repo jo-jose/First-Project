e7e540119a4f09809146257fa4e58d2689bc0674
jo-jose <73060254+jo-jose@users.noreply.github.com>
2020-10-18 19:42:19 +0530

    Add files via upload

---
+++ get-resource-group-details.ps1
@@ -0,0 +1,14 @@
+﻿
+$SubIDs = (Get-AzureRmSubscription).SubscriptionId
+foreach ($sub in $SubIDs) {
+    Set-AzureRmContext -SubscriptionId $sub | Out-Null
+    $SubName = (Get-AzureRmContext).Subscription.SubscriptionName
+    Write-Host "Logged into subscription $SubName.." -ForegroundColor Green
+    $rgs = Get-AzureRmResourceGroup
+    foreach ($rg in $rgs) {
+        $rgName = $rg.ResourceGroupName
+        $filename = $SubName + "_" + $rgName
+        Write-Host "Fetching details of resource group $rgName. ..." -ForegroundColor Yellow
+        $rg >> "$filename.csv"
+    }
+}
